b8e498132b4e1a4958006ad89936efe254939347
Natalya Renee <nbuchwal@andrew.cmu.edu>
2017-02-17 15:25:39 -0500

    Revert "Add article model, relationships and scopes"

--- app/models/article.rb
+++
@@ -1,9 +0,0 @@
-class Article < ActiveRecord::Base
-  belongs_to :category
-
-  validates_presence_of :title
-  validates_presence_of :content
-
-  scope :alphabetical, -> { order('title') }
-  scope :active, -> { where(active: true) }
-end
